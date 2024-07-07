c3d45db548b12f02ddd8be0de34215b94ed83f69
Faruk <97340877+Faruk-Tutkus@users.noreply.github.com>
2024-07-08 01:28:29 +0300

    Update random.js

--- api/random.js
+++ api/random.js
@@ -4,19 +4,12 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import {
   GoogleGenerativeAI,
-  HarmCategory,
-  HarmBlockThreshold,
 } from "@google/generative-ai"
 dotenv.config()
-const PORT = process.env.PORT || 3000;
 const app = express()
 const apiKey = process.env.API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
-const model_score = genAI.getGenerativeModel({
-  model: "gemini-1.5-pro",
-  systemInstruction: "give a percentage score, like a native, for how accurately it translates the given original sentence.",
-});
 const model_random = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
 });
@@ -31,8 +24,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 export default async function handler(req, res) {
-  const { level, topic, originallanguage } = req.body;
-  const prompt = `write a random at ${level} level sentence about ${topic} in ${originallanguage}, just one sentence without translation`
+  const { level, topic, originalLanguage } = req.body;
+  const prompt = `write a random at ${level} level sentence about ${topic} in ${originalLanguage}, just one sentence without translation`
 
   const result = await model_random.generateContent(prompt, generationConfig);
   const response = result.response.text();
